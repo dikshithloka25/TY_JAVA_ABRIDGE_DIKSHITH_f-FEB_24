9d2064ebd1b416d1d7160d684cc0c4fc68b40e45
dikshithloka25 <61976747+dikshithloka25@users.noreply.github.com>
2020-03-10 08:17:12 +0530

    dix

---
+++ Question1.java
@@ -0,0 +1,21 @@
+package com.capgemini.Quiz.day2;
+
+public class Question1 {
+	int y;
+	double x;
+	void add(double a , double b) {
+		
+		x = a + b;
+		
+	}
+	void add(int c , int d) {
+		y = c + d;
+		
+	}
+	Question1(){
+		this.x = 0;
+		this.y = 0;
+		
+	}
+}
+
